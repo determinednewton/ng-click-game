5886c80da303797b2954cb9c2a7162bad09fa2bb
Robert Todea <robert.todea@e-spres-oh.com>
2018-10-16 11:09:56 +0300

    Basis for the game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ng-dom-manipulation';
+  number: number;
+  result: string;
+
+  constructor() {
+
+  }
+
+  start() {
+    timer(1000, 4000).subscribe((n) => {
+      this.number = n;
+    });
+  }
+
+  isMatch(type) {
+    let match = false;
+    if (type === 'even') {
+      match = (this.number % 2 === 0);
+    } else if (type === 'odd') {
+      match = (this.number % 2 === 1);
+    }
+    this.result = `${this.number} is ${type}: ` + (match ? 'success' : 'false');
+  }
 }
